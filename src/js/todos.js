dd6d324c919f9b8dbb95acdf2b4d4347d09ea420
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-04-13 16:02:17 +0700

    Create add todo feature

--- src/js/todos.js
+++ src/js/todos.js
@@ -1,13 +1,19 @@
 window.onload = showAllTodos()
 
+const addButton = document.querySelector('.add')
+addButton.addEventListener('click', addTodo)
+
+const activeUser = JSON.parse(localStorage.getItem('loginUser'))
+
 function showAllTodos(){
+  document.querySelector('#todo-list ul').innerHTML = ''
+  
   const url = 'https://5e9407d7c7393c0016de4cfc.mockapi.io/todos'
 
   fetch(url)
     .then(response => response.json())
     .then(results => {
-      const activeUser = JSON.parse(localStorage.getItem('loginUser'))
-
+      // const activeUser = JSON.parse(localStorage.getItem('loginUser'))
       const userTodos = results.filter(result => {
         return result.userId === activeUser.id
       })
@@ -35,6 +41,34 @@ function showAllTodos(){
         
         document.querySelector('#todo-list ul').appendChild(li)
       })
+
+  
     })
     .catch(error => console.log(error))
 }
+
+function addTodo(){
+  const data = {
+    todo: document.getElementById('todo').value,
+    userId: activeUser.id
+  }
+
+  const url = 'https://5e9407d7c7393c0016de4cfc.mockapi.io/todos'
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then(todo => showAllTodos())
+    .catch(error => console.log(error))
+}
+
+
+
+
+
